9b05887a352cbd915f98ce6b9dfefcf6da562bc7
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2023-04-13 20:20:32 +0300

    added functionality and season tables

---
+++ data/season.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class Season(SqlAlchemyBase):
+    __tablename__ = 'season'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    season = sqlalchemy.Column(sqlalchemy.String)
