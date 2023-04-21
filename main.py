ec02f72ef993d348bc89269dd0d060f51c1821c4
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2023-04-21 09:56:19 +0300

    redesigned adding clothes page

--- main.py
+++ main.py
@@ -327,6 +327,7 @@ def edit_profile():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    session['visits_count'] = 0
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
